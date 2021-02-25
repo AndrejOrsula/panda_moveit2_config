667266d8ba3fc0bbaad23f6e9d4ac3e05b2ebf83
Andrej Orsula <orsula.andrej@gmail.com>
2021-02-25 18:50:08 +0100

    Add log level launch argument - Also, use standard single/double quotes
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- launch/move_group_action_server.launch.py
+++ launch/move_group_action_server.launch.py
@@ -14,7 +14,7 @@ def load_file(package_name, file_path):
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
-        with open(absolute_file_path, "r") as file:
+        with open(absolute_file_path, 'r') as file:
             return file.read()
     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
@@ -25,7 +25,7 @@ def load_yaml(package_name, file_path):
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
-        with open(absolute_file_path, "r") as file:
+        with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
@@ -33,63 +33,69 @@ def load_yaml(package_name, file_path):
 
 def generate_launch_description():
     # Launch Arguments
-    use_sim_time = LaunchConfiguration("use_sim_time", default=False)
-    config_rviz2 = LaunchConfiguration("config_rviz2", default=os.path.join(get_package_share_directory("panda_moveit2_config"),
-                                                                            "launch", "rviz.rviz"))
+    use_sim_time = LaunchConfiguration('use_sim_time', default=False)
+    config_rviz2 = LaunchConfiguration('config_rviz2', default=os.path.join(get_package_share_directory('panda_moveit2_config'),
+                                                                            'launch', 'rviz.rviz'))
+    log_level = LaunchConfiguration('log_level', default='error')
 
     # URDF
-    robot_urdf_config = load_file("panda_ign",
-                                  "urdf/panda.urdf")
-    robot_description = {"robot_description": robot_urdf_config}
+    robot_urdf_config = load_file('panda_ign',
+                                  'urdf/panda.urdf')
+    robot_description = {'robot_description': robot_urdf_config}
 
     # SRDF
-    robot_srdf = load_file("panda_moveit2_config",
-                           "srdf/panda.srdf")
-    robot_description_semantic = {"robot_description_semantic": robot_srdf}
+    robot_srdf = load_file('panda_moveit2_config',
+                           'srdf/panda.srdf')
+    robot_description_semantic = {'robot_description_semantic': robot_srdf}
 
     # Kinematics
-    kinematics = load_yaml("panda_moveit2_config",
-                           "config/kinematics.yaml")
+    kinematics = load_yaml('panda_moveit2_config',
+                           'config/kinematics.yaml')
 
     # Joint limits
-    joint_limits_yaml = load_yaml("panda_moveit2_config",
-                           "config/joint_limits.yaml")
-    joint_limits = {"robot_description_planning": joint_limits_yaml}
+    joint_limits_yaml = load_yaml('panda_moveit2_config',
+                                  'config/joint_limits.yaml')
+    joint_limits = {'robot_description_planning': joint_limits_yaml}
 
     # Planning
-    ompl_yaml = load_yaml("panda_moveit2_config",
-                          "config/ompl_planning.yaml")
-    planning = {"move_group": {
-        "planning_plugin": "ompl_interface/OMPLPlanner",
-        "request_adapters": """default_planner_request_adapters/AddTimeParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-        "start_state_max_bounds_error": 0.1}}
+    ompl_yaml = load_yaml('panda_moveit2_config',
+                          'config/ompl_planning.yaml')
+    planning = {'move_group': {
+        'planning_plugin': 'ompl_interface/OMPLPlanner',
+        'request_adapters': 'default_planner_request_adapters/AddTimeParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints',
+        'start_state_max_bounds_error': 0.1}}
 
     # Trajectory Execution
-    trajectory_execution = {"allow_trajectory_execution": False,
-                            "moveit_manage_controllers": False}
+    trajectory_execution = {'allow_trajectory_execution': False,
+                            'moveit_manage_controllers': False}
 
     # Planning Scene
-    planning_scene_monitor_parameters = {"publish_planning_scene": True,
-                                         "publish_geometry_updates": True,
-                                         "publish_state_updates": True,
-                                         "publish_transforms_updates": True}
+    planning_scene_monitor_parameters = {'publish_planning_scene': True,
+                                         'publish_geometry_updates': True,
+                                         'publish_state_updates': True,
+                                         'publish_transforms_updates': True}
 
     return LaunchDescription([
         # Launch Arguments
         DeclareLaunchArgument(
-            "use_sim_time",
+            'use_sim_time',
             default_value=use_sim_time,
             description="If true, use simulated clock"),
         DeclareLaunchArgument(
-            "config_rviz2",
+            'config_rviz2',
             default_value=config_rviz2,
             description="Path to config for RViz2"),
+        DeclareLaunchArgument(
+            'log_level',
+            default_value=log_level,
+            description="Log level of all nodes launched by this script"),
 
         # Start move_group action server
-        Node(package="moveit_ros_move_group",
-             executable="move_group",
-             name="move_group",
-             output="screen",
+        Node(package='moveit_ros_move_group',
+             executable='move_group',
+             name='move_group',
+             output='screen',
+             arguments=['--ros-args', '--log-level', log_level],
              parameters=[robot_description,
                          robot_description_semantic,
                          kinematics,
@@ -98,17 +104,18 @@ def generate_launch_description():
                          ompl_yaml,
                          trajectory_execution,
                          planning_scene_monitor_parameters,
-                         {"use_sim_time": use_sim_time}]),
+                         {'use_sim_time': use_sim_time}]),
 
         # RViz2
-        Node(package="rviz2",
-             executable="rviz2",
-             name="rviz2",
-             output="log",
-             arguments=["--display-config", config_rviz2],
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             output='log',
+             arguments=['--display-config', config_rviz2,
+                        '--ros-args', '--log-level', log_level],
              parameters=[robot_description,
                          robot_description_semantic,
                          kinematics,
                          planning,
-                         {"use_sim_time": use_sim_time}])
+                         {'use_sim_time': use_sim_time}])
     ])
--- launch/move_group_fake_control.launch.py
+++ launch/move_group_fake_control.launch.py
@@ -14,7 +14,7 @@ def load_file(package_name, file_path):
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
-        with open(absolute_file_path, "r") as file:
+        with open(absolute_file_path, 'r') as file:
             return file.read()
     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
@@ -25,7 +25,7 @@ def load_yaml(package_name, file_path):
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
-        with open(absolute_file_path, "r") as file:
+        with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
@@ -33,68 +33,68 @@ def load_yaml(package_name, file_path):
 
 def generate_launch_description():
     # Launch Arguments
-    use_sim_time = LaunchConfiguration("use_sim_time", default=False)
-    config_rviz2 = LaunchConfiguration("config_rviz2", default=os.path.join(get_package_share_directory("panda_moveit2_config"),
-                                                                            "launch", "rviz_interactive.rviz"))
+    use_sim_time = LaunchConfiguration('use_sim_time', default=False)
+    config_rviz2 = LaunchConfiguration('config_rviz2', default=os.path.join(get_package_share_directory('panda_moveit2_config'),
+                                                                            'launch', 'rviz_interactive.rviz'))
 
     # URDF
-    robot_urdf_config = load_file("panda_ign",
-                                  "urdf/panda.urdf")
-    robot_description = {"robot_description": robot_urdf_config}
+    robot_urdf_config = load_file('panda_ign',
+                                  'urdf/panda.urdf')
+    robot_description = {'robot_description': robot_urdf_config}
 
     # SRDF
-    robot_srdf = load_file("panda_moveit2_config",
-                           "srdf/panda.srdf")
-    robot_description_semantic = {"robot_description_semantic": robot_srdf}
+    robot_srdf = load_file('panda_moveit2_config',
+                           'srdf/panda.srdf')
+    robot_description_semantic = {'robot_description_semantic': robot_srdf}
 
     # Kinematics
-    kinematics = load_yaml("panda_moveit2_config",
-                           "config/kinematics.yaml")
+    kinematics = load_yaml('panda_moveit2_config',
+                           'config/kinematics.yaml')
 
     # Planning
-    ompl_yaml = load_yaml("panda_moveit2_config",
-                          "config/ompl_planning.yaml")
-    planning = {"move_group": {
-        "planning_plugin": "ompl_interface/OMPLPlanner",
-        "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-        "start_state_max_bounds_error": 0.1}}
-    planning["move_group"].update(ompl_yaml)
+    ompl_yaml = load_yaml('panda_moveit2_config',
+                          'config/ompl_planning.yaml')
+    planning = {'move_group': {
+        'planning_plugin': 'ompl_interface/OMPLPlanner',
+        'request_adapters': 'default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints',
+        'start_state_max_bounds_error': 0.1}}
+    planning['move_group'].update(ompl_yaml)
 
     # Controllers
-    controllers_yaml = load_yaml("panda_moveit2_config",
-                                 "config/fake_control/controllers.yaml")
-    moveit_controllers = {"moveit_simple_controller_manager": controllers_yaml,
-                          "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager"}
+    controllers_yaml = load_yaml('panda_moveit2_config',
+                                 'config/fake_control/controllers.yaml')
+    moveit_controllers = {'moveit_simple_controller_manager': controllers_yaml,
+                          'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
 
     # Trajectory Execution
-    trajectory_execution = {"allow_trajectory_execution": True,
-                            "moveit_manage_controllers": True,
-                            "trajectory_execution.allowed_execution_duration_scaling": 1.2,
-                            "trajectory_execution.allowed_goal_duration_margin": 0.5,
-                            "trajectory_execution.allowed_start_tolerance": 0.01}
+    trajectory_execution = {'allow_trajectory_execution': True,
+                            'moveit_manage_controllers': True,
+                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
+                            'trajectory_execution.allowed_start_tolerance': 0.01}
 
     # Planning Scene
-    planning_scene_monitor_parameters = {"publish_planning_scene": True,
-                                         "publish_geometry_updates": True,
-                                         "publish_state_updates": True,
-                                         "publish_transforms_updates": True}
+    planning_scene_monitor_parameters = {'publish_planning_scene': True,
+                                         'publish_geometry_updates': True,
+                                         'publish_state_updates': True,
+                                         'publish_transforms_updates': True}
 
     return LaunchDescription([
         # Launch Arguments
         DeclareLaunchArgument(
-            "use_sim_time",
+            'use_sim_time',
             default_value=use_sim_time,
             description="If true, use simulated clock"),
         DeclareLaunchArgument(
-            "config_rviz2",
+            'config_rviz2',
             default_value=config_rviz2,
             description="Path to config for RViz2"),
 
         # Start move_group action server
-        Node(package="moveit_ros_move_group",
-             executable="move_group",
-             name="move_group",
-             output="screen",
+        Node(package='moveit_ros_move_group',
+             executable='move_group',
+             name='move_group',
+             output='screen',
              parameters=[robot_description,
                          robot_description_semantic,
                          kinematics,
@@ -102,46 +102,46 @@ def generate_launch_description():
                          trajectory_execution,
                          moveit_controllers,
                          planning_scene_monitor_parameters,
-                         {"use_sim_time": use_sim_time}]),
+                         {'use_sim_time': use_sim_time}]),
 
         # Static TF
-        Node(package="tf2_ros",
-             executable="static_transform_publisher",
-             name="static_transform_publisher_world_panda",
-             output="log",
-             arguments=["0.0", "0.0", "0.0",
-                        "0.0", "0.0", "0.0",
-                        "world", "panda_link0"],
-             parameters=[{"use_sim_time": use_sim_time}]),
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             name='static_transform_publisher_world_panda',
+             output='log',
+             arguments=['0.0', '0.0', '0.0',
+                        '0.0', '0.0', '0.0',
+                        'world', 'panda_link0'],
+             parameters=[{'use_sim_time': use_sim_time}]),
 
         # Robot state publisher
-        Node(package="robot_state_publisher",
-             executable="robot_state_publisher",
-             name="robot_state_publisher",
-             output="screen",
+        Node(package='robot_state_publisher',
+             executable='robot_state_publisher',
+             name='robot_state_publisher',
+             output='screen',
              parameters=[robot_description,
-                         {"use_sim_time": use_sim_time}]),
+                         {'use_sim_time': use_sim_time}]),
 
         # Fake joint driver
-        Node(package="fake_joint_driver",
-             executable="fake_joint_driver_node",
-             parameters=[{"controller_name": "panda_arm_controller"},
+        Node(package='fake_joint_driver',
+             executable='fake_joint_driver_node',
+             parameters=[{'controller_name': 'panda_arm_controller'},
                          robot_description,
                          os.path.join(get_package_share_directory(
-                             "panda_moveit2_config"), "config", "fake_control", "panda_arm_controller.yaml"),
+                             'panda_moveit2_config'), 'config', 'fake_control', 'panda_arm_controller.yaml'),
                          os.path.join(get_package_share_directory(
-                             "panda_moveit2_config"), "config", "fake_control", "start_position.yaml"),
-                         {"use_sim_time": use_sim_time}]),
+                             'panda_moveit2_config'), 'config', 'fake_control', 'start_position.yaml'),
+                         {'use_sim_time': use_sim_time}]),
 
         # RViz2
-        Node(package="rviz2",
-             executable="rviz2",
-             name="rviz2",
-             output="log",
-             arguments=["--display-config", config_rviz2],
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             output='log',
+             arguments=['--display-config', config_rviz2],
              parameters=[robot_description,
                          robot_description_semantic,
                          kinematics,
                          planning,
-                         {"use_sim_time": use_sim_time}])
+                         {'use_sim_time': use_sim_time}])
     ])
